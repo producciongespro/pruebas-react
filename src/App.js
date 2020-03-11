e2472b61a0b55cf169d540c72b72e4d8ce6198e2
lchaconc <luis.chacon.campos@gmail.com>
2020-03-11 10:59:47 -0600

    Fase 2 generación de gráfico
    -Fitrado de datos
    - Creación de componente Chart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from './componentes/Menu';
 import Grafico1 from './componentes/Grafico1';
 
--- src/componentes/Grafico1.js
+++ src/componentes/Grafico1.js
@@ -1,25 +1,42 @@
 import React, {useState, useEffect } from 'react';
+import Chart from '../componentes/Chart';
 const API_URL= "https://nataliia-radina.github.io/react-vis-example/";
+
 
 function Grafico1 () {
 
-    const {datosJson, setDatosJson} = useState(null);
+    const [datosJson, setDatosJson] = useState(null);
 
     async function obtenerDatos () {
-        const tmpJson = await fetch(API_URL);
-        console.log(tmpJson);       
+        let response = await fetch(API_URL);
+        let rawJson =  await response.json()
+        console.log("rawJson.results",rawJson.results);        
+        //setDatosJson(rawJson.results);       
+        let resultados = await rawJson.results.filter((r)=>{
+            //return r.name === 'JavaScript';
+            return r.name === 'Pascal';
+        });
+        console.log("resultados",resultados); 
+        setDatosJson(resultados);       
     }
 
     useEffect (()=>{
-        obtenerDatos();
+        obtenerDatos();       
+    },[] );
+
+    useEffect(()=>{
+        console.log("datosJson",datosJson);
     })
 
     return (
-        <div className="col-12">
-            <div className="alert alert-success"> 
-                Gráfico 1
+        
+         datosJson &&
+         (
+            <div className="col-12">
+                <Chart data={datosJson} />
             </div>
-        </div>
+         )
+        
     )
     
 }
